ce3d382b13d5ec58a508377c40a0f4281d4b7f56
Dominic Go <dominicstop@gmail.com>
2020-10-11 07:45:42 +0800

    ✨ Example - Changed tsx to js

--- example/src/App.js
+++ example/src/App.js
@@ -3,7 +3,7 @@ import { StyleSheet, View, Text } from 'react-native';
 import IosModal from 'react-native-ios-modal';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [result, setResult] = React.useState();
 
   React.useEffect(() => {
     IosModal.multiply(3, 7).then(setResult);
