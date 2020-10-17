63b7e6e59b572754c4be281ae89403b7c076ba5b
Dominic Go <dominicstop@gmail.com>
2020-10-17 10:39:57 +0800

    🐞 Minor Fix

--- src/ModalView.js
+++ src/ModalView.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+export const AvailableBlurEffectStyles   = [];
+export const AvailablePresentationStyles = [];
+
+
 export class ModalView extends React.PureComponent {
   render(){
     return(
