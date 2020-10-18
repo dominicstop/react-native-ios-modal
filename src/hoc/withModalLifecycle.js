5f49f5215c62cc04bc122f899aee2905355661ba
Dominic Go <dominicstop@gmail.com>
2020-10-18 15:27:57 +0800

    ✨ Updated `withModalLifecycle`
    Summary: replaced hard coded implementation

--- src/hoc/withModalLifecycle.js
+++ src/hoc/withModalLifecycle.js
@@ -4,6 +4,8 @@ import { ModalContext   } from '../context/ModalContext';
 import { ModalEventKeys } from '../constants/Enums';
 
 
+const eventKeys = Object.keys(ModalEventKeys);
+
 export function withModalLifecycle(WrappedComponent){
   return class extends React.PureComponent {
     static contextType = ModalContext;
@@ -12,29 +14,27 @@ export function withModalLifecycle(WrappedComponent){
       const modalContext = this.context;
       const modalEmitter = modalContext.getEmitterRef();
 
-      modalEmitter.addListener(ModalEventKeys.onModalBlur          , this._handleOnModalBlur          );
-      modalEmitter.addListener(ModalEventKeys.onModalFocus         , this._handleOnModalFocus         );
-      modalEmitter.addListener(ModalEventKeys.onModalShow          , this._handleOnModalShow          );
-      modalEmitter.addListener(ModalEventKeys.onModalDismiss       , this._handleOnModalDismiss       );
-      modalEmitter.addListener(ModalEventKeys.onModalDidDismiss    , this._handleOnModalDidDismiss    );
-      modalEmitter.addListener(ModalEventKeys.onModalWillDismiss   , this._handleOnModalWillDismiss   );
-      modalEmitter.addListener(ModalEventKeys.onModalAttemptDismiss, this._handleOnModalAttemptDismiss);
+      for (const eventKey of eventKeys) {
+        const functionName = `handle-${eventKey}`;
+        
+        this[functionName] = (event) => {
+          this.childRef?.onModalShow?.(event);
+        };
+
+        modalEmitter.addListener(eventKey, this[functionName]);
+      };
     };
 
     componentWillUnmount(){
       const modalContext = this.context;
       const modalEmitter = modalContext.getEmitterRef();
 
-      modalEmitter.removeListener(ModalEventKeys.onModalBlur          , this._handleOnModalBlur          );
-      modalEmitter.removeListener(ModalEventKeys.onModalFocus         , this._handleOnModalFocus         );
-      modalEmitter.removeListener(ModalEventKeys.onModalShow          , this._handleOnModalShow          );
-      modalEmitter.removeListener(ModalEventKeys.onModalDismiss       , this._handleOnModalDismiss       );
-      modalEmitter.removeListener(ModalEventKeys.onModalDidDismiss    , this._handleOnModalDidDismiss    );
-      modalEmitter.removeListener(ModalEventKeys.onModalWillDismiss   , this._handleOnModalWillDismiss   );
-      modalEmitter.removeListener(ModalEventKeys.onModalAttemptDismiss, this._handleOnModalAttemptDismiss);
+      for (const eventKey of eventKeys) {
+        const functionName = `handle-${eventKey}`;
+        modalEmitter.removeListener(eventKey, this[functionName]);
+      };
     };
 
-    //#region - Event Handlers
     _handleChildRef = (node) => {
       const { ref } = this.props;
       
@@ -48,35 +48,6 @@ export function withModalLifecycle(WrappedComponent){
         ref.current = node;
       };
     };
-
-    _handleOnModalBlur = (event) => {
-      this.childRef?.onModalBlur?.(event);
-    };
-
-    _handleOnModalFocus = (event) => {
-      this.childRef?.onModalFocus?.();
-    };
-
-    _handleOnModalShow = (event) => {
-      this.childRef?.onModalShow?.(event);
-    };
-
-    _handleOnModalDismiss = (event) => {
-      this.childRef?.onModalDismiss?.(event);
-    };
-
-    _handleOnModalDidDismiss = (event) => {
-      this.childRef?.onModalDidDismiss?.(event);
-    };
-
-    _handleOnModalWillDismiss = (event) => {
-      this.childRef?.onModalWillDismiss?.(event);
-    };
-
-    _handleOnModalAttemptDismiss = (event) => {
-      this.childRef?.onModalAttemptDismiss?.(event);
-    };
-    //#endregion
 
     render(){
       const props = this.props;
