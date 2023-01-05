1e65418fd993fdc8bb5fd7632b9f0fea0979c633
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-01-05 16:43:47 +0800

    ⭐️ Impl: Types - Add `UIBlurEffectStyles`
    TODO:2023-03-04-04-20-46 - Library Typescript Re-Write

---
+++ src/types/UIBlurEffectStyles.ts
@@ -0,0 +1,23 @@
+/** Maps to `UIBlurEffect.Style` */
+export type UIBlurEffectStyles =
+  | 'systemUltraThinMaterial'
+  | 'systemThinMaterial'
+  | 'systemMaterial'
+  | 'systemThickMaterial'
+  | 'systemChromeMaterial'
+  | 'systemUltraThinMaterialLight'
+  | 'systemThinMaterialLight'
+  | 'systemMaterialLight'
+  | 'systemThickMaterialLight'
+  | 'systemChromeMaterialLight'
+  | 'systemUltraThinMaterialDark'
+  | 'systemThinMaterialDark'
+  | 'systemMaterialDark'
+  | 'systemThickMaterialDark'
+  | 'systemChromeMaterialDark'
+  | 'extraLight'
+  | 'light'
+  | 'dark'
+  | 'extraDark'
+  | 'regular'
+  | 'prominent';
