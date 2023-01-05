26193f7706d52bfd31a8d916d4c6917601b76364
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-01-05 16:42:49 +0800

    ⭐️ Impl: Types - Add `UIModalTypes`

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ export * from './ModalView';
 export * from './ModalViewModule';
 export * from './constants/Enums';
 export * from './context/ModalContext';
-export * from './hoc/withModalLifecycle';
+export * from './hoc/withModalLifecycle';export * from './types/UIModalTypes';
+
+//
---
+++ src/types/UIModalTypes.ts
@@ -0,0 +1,19 @@
+export type UIModalTransitionStyle =
+  | 'coverVertical'
+  | 'flipHorizontal'
+  | 'crossDissolve'
+  | 'partialCurl';
+
+export type UIModalPresentationStyle =
+  | 'automatic'
+  | 'none'
+  | 'fullScreen'
+  | 'pageSheet'
+  | 'formSheet'
+  | 'currentContext'
+  | 'custom'
+  | 'overFullScreen'
+  | 'overCurrentContext'
+  | 'popover'
+  | 'blurOverFullScreen';
+
