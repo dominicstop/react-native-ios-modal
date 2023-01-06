e476081e80bdd76f73fc9fa9e3a33e962a5561b0
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-01-06 20:33:20 +0800

    🛠 Refactor: Move `ModalViewModule`
    TODO:2023-03-04-05-25-44 - Library Cleanup
    
    Summary: Move `ModalViewModule.js` from `src` to `src/native_modules`.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 export * from './ModalView';
-export * from './ModalViewModule';
+
 export * from './constants/Enums';
 export * from './context/ModalContext';
 export * from './hoc/withModalLifecycle';
 
 export * from './native_components/RNIModalView';
+export * from './native_modules/ModalViewModule';
 
 export * from './types/UIModalTypes';
