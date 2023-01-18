965338dd62c33c0b79d39418a2ce88bac552a167
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-01-18 10:27:27 +0800

    💄 Gloss: Fix Lint/Prettier Warnings

--- src/constants/Enums.js
+++ src/constants/Enums.js
@@ -1,61 +1,66 @@
 export const UIBlurEffectStyles = {
-  // Adaptable Styles -------------------------------
-  systemUltraThinMaterial: "systemUltraThinMaterial",
-  systemThinMaterial     : "systemThinMaterial"     ,
-  systemMaterial         : "systemMaterial"         ,
-  systemThickMaterial    : "systemThickMaterial"    ,
-  systemChromeMaterial   : "systemChromeMaterial"   ,
-  // Light Styles ---------------------------------------------
-  systemMaterialLight         : "systemMaterialLight"         ,
-  systemThinMaterialLight     : "systemThinMaterialLight"     ,
-  systemUltraThinMaterialLight: "systemUltraThinMaterialLight",
-  systemThickMaterialLight    : "systemThickMaterialLight"    ,
-  systemChromeMaterialLight   : "systemChromeMaterialLight"   ,
-  // Dark Styles --------------------------------------------
-  systemChromeMaterialDark   : "systemChromeMaterialDark"   ,
-  systemMaterialDark         : "systemMaterialDark"         ,
-  systemThickMaterialDark    : "systemThickMaterialDark"    ,
-  systemThinMaterialDark     : "systemThinMaterialDark"     ,
-  systemUltraThinMaterialDark: "systemUltraThinMaterialDark",
-  // Additional Styles ----
-  regular   : "regular"   ,
-  prominent : "prominent" ,
-  light     : "light"     ,
-  extraLight: "extraLight",
-  dark      : "dark"      ,
+  // Adaptable Styles
+  systemUltraThinMaterial: 'systemUltraThinMaterial',
+  systemThinMaterial: 'systemThinMaterial',
+  systemMaterial: 'systemMaterial',
+  systemThickMaterial: 'systemThickMaterial',
+  systemChromeMaterial: 'systemChromeMaterial',
+
+  // Light Styles
+  systemMaterialLight: 'systemMaterialLight',
+  systemThinMaterialLight: 'systemThinMaterialLight',
+  systemUltraThinMaterialLight: 'systemUltraThinMaterialLight',
+  systemThickMaterialLight: 'systemThickMaterialLight',
+  systemChromeMaterialLight: 'systemChromeMaterialLight',
+
+  // Dark Styles
+  systemChromeMaterialDark: 'systemChromeMaterialDark',
+  systemMaterialDark: 'systemMaterialDark',
+  systemThickMaterialDark: 'systemThickMaterialDark',
+  systemThinMaterialDark: 'systemThinMaterialDark',
+  systemUltraThinMaterialDark: 'systemUltraThinMaterialDark',
+
+  // Additional Styles
+  regular: 'regular',
+  prominent: 'prominent',
+  light: 'light',
+  extraLight: 'extraLight',
+  dark: 'dark',
 };
 
 export const UIModalPresentationStyles = {
-  automatic         : 'automatic'     ,
-  fullScreen        : 'fullScreen'    ,
-  pageSheet         : 'pageSheet'     ,
-  formSheet         : 'formSheet'     ,
-  overFullScreen    : 'overFullScreen',
+  automatic: 'automatic',
+  fullScreen: 'fullScreen',
+  pageSheet: 'pageSheet',
+  formSheet: 'formSheet',
+  overFullScreen: 'overFullScreen',
+
   /* NOT SUPPORTED
-  none              : 'none'              ,
-  currentContext    : 'currentContext'    ,
-  custom            : 'custom'            ,
+  none: 'none',
+  currentContext: 'currentContext',
+  custom: 'custom',
   overCurrentContext: 'overCurrentContext',
-  popover           : 'popover'           ,
+  popover: 'popover',
   blurOverFullScreen: 'blurOverFullScreen',
   */
 };
 
 export const UIModalTransitionStyles = {
-  coverVertical : 'coverVertical' ,
-  crossDissolve : 'crossDissolve' ,
+  coverVertical: 'coverVertical',
+  crossDissolve: 'crossDissolve',
   flipHorizontal: 'flipHorizontal',
+
   /* NOT SUPPORTED
-  partialCurl   : 'partialCurl'   ,
+  partialCurl: 'partialCurl',
   */
 };
 
 export const ModalEventKeys = {
-  onModalBlur          : 'onModalBlur'          ,
-  onModalFocus         : 'onModalFocus'         ,
-  onModalShow          : 'onModalShow'          ,
-  onModalDismiss       : 'onModalDismiss'       ,
-  onModalDidDismiss    : 'onModalDidDismiss'    ,
-  onModalWillDismiss   : 'onModalWillDismiss'   ,
+  onModalBlur: 'onModalBlur',
+  onModalFocus: 'onModalFocus',
+  onModalShow: 'onModalShow',
+  onModalDismiss: 'onModalDismiss',
+  onModalDidDismiss: 'onModalDidDismiss',
+  onModalWillDismiss: 'onModalWillDismiss',
   onModalAttemptDismiss: 'onModalAttemptDismiss',
 };
