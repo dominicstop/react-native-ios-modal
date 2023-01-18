c4934e5c714e71a333586addf8a34f485dbdb1d7
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-01-18 12:18:55 +0800

    🛠 Refactor: Remove `withModalLifecycle`
    TODO:2023-03-04-04-20-46 - Library Typescript Re-Write

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ export * from './module/ModalViewModule';
 
 export * from './constants/Enums';
 export * from './context/ModalContext';
-export * from './hoc/withModalLifecycle';
 
 export * from './native_components/RNIModalView';
 export * from './native_modules/RNIModalViewModule';
