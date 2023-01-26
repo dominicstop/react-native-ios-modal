cbac5a702dc87bcbf097a887af847cc8425c68c2
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-01-26 09:33:23 +0800

    🛠 Refactor: Ex - Fix Relative Import
    TODO:2023-03-04-04-39-44 - Import + Re–write Example Components

--- example/src/components/ObjectPropertyDisplay.tsx
+++ example/src/components/ObjectPropertyDisplay.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { StyleSheet, View, Text, ViewStyle } from 'react-native';
 
-import * as Colors from '../../constants/Colors';
+import * as Colors from '../constants/Colors';
 
 /**
  * ```
