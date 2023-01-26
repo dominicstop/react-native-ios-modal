bf8fbfeb1d1067eb85a9163e8871f573b26d3c25
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-01-26 17:19:02 +0800

    ⚙️ Chore: Update Config

--- react-native.config.js
+++ react-native.config.js
@@ -1,17 +1,8 @@
-const ios = require('@react-native-community/cli-platform-ios');
-
 module.exports = {
   project: {
     android: null,
     ios: {
       project: './ios/IosModal.xcodeproj',
     },
-  },
-  platforms: {
-    ios: {
-      projectConfig: ios.projectConfig,
-      dependencyConfig: ios.dependencyConfig,
-    },
-    android: null,
   },
 };
