90fc11a1d2bc2b52b2ae1546e807a28519871114
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-03-03 18:40:41 +0800

    💄 Gloss: Update Code Layout

--- src/context/ModalContext.ts
+++ src/context/ModalContext.ts
@@ -1,14 +1,18 @@
 import React from 'react';
 import type { ModalView } from 'src/components/ModalView';
 
+// prettier-ignore
 export type ModalContextType = Partial<
+  // `ModalView` Methods #1
   Pick<
-    ModalView,
-    | 'getEmitterRef'
-    | 'setVisibility'
-    | 'setEnableSwipeGesture'
-    | 'setIsModalInPresentation'
-  > & {
+      ModalView,
+      | 'getEmitterRef'
+      | 'setVisibility'
+      | 'setEnableSwipeGesture'
+      | 'setIsModalInPresentation'
+    >
+  // `ModalView` Methods #2
+  & {
     getModalRef: ModalView['_handleGetModalRef'];
   }
 >;
