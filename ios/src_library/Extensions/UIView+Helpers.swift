13cf593af8fcd4f382170de40fa35b9952eef252
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-03-24 01:14:06 +0800

    🛠 Refactor: Update `UIView+Helpers`
    Related:
    * TODO:2023-03-04-06-34-28 - Library Native Cleanup
    
    Summary:
    * Clean-up/re-write extension function `UIView.removeAllConstraints` from `UIView+Helpers`.
    * Rename `UIView.removeAllConstraints` to `removeAllAncestorConstraints`.

--- ios/src_library/Extensions/UIView+Helpers.swift
+++ ios/src_library/Extensions/UIView+Helpers.swift
@@ -12,25 +12,41 @@ extension UIView {
     return nil
   };
   
-  // From: https://stackoverflow.com/questions/24418884/remove-all-constraints-affecting-a-uiview
-  // After it's done executing your view remains where it was because it creates autoresizing
-  // constraints. When I don't do this the view usually disappears. Additionally, it doesn't just
-  // remove constraints from superview but traversing all the way up as there may be constraints affecting it in ancestor views.
-  public func removeAllConstraints() {
-    var _superview = self.superview;
+  /// Remove all ancestor constraints that are affecting this view instance
+  ///
+  /// Note: 2023-03-24-00-39-51
+  ///
+  /// * From: https://stackoverflow.com/questions/24418884/remove-all-constraints-affecting-a-uiview
+  ///
+  /// * After it's done executing, your view remains where it was because it
+  ///   creates autoresizing constraints.
+  ///
+  /// * When I don't do this the view usually disappears.
+  ///
+  /// * Additionally, it doesn't just remove constraints from it's superview,
+  ///   but in addition, it also climbs the view hierarchy, and removes all the
+  ///   constraints affecting the current view instance that came from an
+  ///   ancestor view.
+  ///
+  public func removeAllAncestorConstraints() {
+    var ancestorView = self.superview;
     
-    while let superview = _superview {
-      for constraint in superview.constraints {
-        if let first = constraint.firstItem as? UIView, first == self {
-          superview.removeConstraint(constraint)
+    // Climb the view hierarchy until there are no more parent views...
+    while ancestorView != nil {
+      for ancestorConstraint in ancestorView!.constraints {
+        
+        let constraintItems = [
+          ancestorConstraint.firstItem,
+          ancestorConstraint.secondItem
+        ];
+        
+        constraintItems.forEach {
+          guard ($0 as? UIView) === self else { return };
+          ancestorView!.removeConstraint(ancestorConstraint);
         };
-
-        if let second = constraint.secondItem as? UIView, second == self {
-          superview.removeConstraint(constraint);
-        };
+        
+        ancestorView = ancestorView!.superview;
       };
-      
-      _superview = superview.superview;
     };
     
     self.removeConstraints(self.constraints);
