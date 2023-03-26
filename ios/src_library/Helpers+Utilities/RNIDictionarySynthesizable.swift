06b2ecc654808cb805e5b9f2894a2b89e6a52ed7
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-03-27 01:52:50 +0800

    💫 Update: `RNIDictionarySynthesizable`
    Related:
    * TODO:2023-03-04-06-34-28 - Library Native Cleanup

--- ios/src_library/Helpers+Utilities/RNIDictionarySynthesizable.swift
+++ ios/src_library/Helpers+Utilities/RNIDictionarySynthesizable.swift
@@ -7,24 +7,72 @@
 
 import Foundation
 
+
 public protocol RNIDictionarySynthesizable {
+  
+  /// The names/identifiers of the property to be ignored when
+  /// `synthesizedDictionary` is created.
+  ///
+  static var synthesizedDictionaryIgnore: [String] { get };
+  
+  /// The key path to the property that will be inlined/"squashed together" into
+  /// `synthesizedDictionary`.
+  ///
+  static var synthesizedDictionaryInlinedProperties: [PartialKeyPath<Self>] { get };
+  
+  /// A map of the property names and their respective values
   var synthesizedDictionary: Dictionary<String, Any> { get };
+
 };
 
 extension RNIDictionarySynthesizable {
+  
+  public static var synthesizedDictionaryIgnore: [String] {
+    [];
+  };
+  
+  public static var synthesizedDictionaryInlinedProperties: [PartialKeyPath<Self>] {
+    [];
+  };
+  
   public var synthesizedDictionary: Dictionary<String, Any> {
     let mirror = Mirror(reflecting: self);
     let properties = mirror.children;
     
+    #if DEBUG
+    for propertyKeyToIgnore in Self.synthesizedDictionaryIgnore {
+      if !properties.contains(where: { $0.label == propertyKeyToIgnore }) {
+        fatalError(
+            "Invalid value of '\(propertyKeyToIgnore)' in "
+          + "'synthesizedDictionaryIgnore' for '\(Self.self)' - "
+          + "No property named '\(propertyKeyToIgnore)' in '\(Self.self)'"
+        );
+      };
+    };
+    #endif
+    
     let propertyValueMap = properties.lazy.map { (
-      property: String?, value: Any) -> (String, Any)? in
+      propertyKey: String?, value: Any) -> (String, Any)? in
       
-      guard let property = property else { return nil }
-      return (property, value)
+      guard let propertyKey = propertyKey,
+            !Self.synthesizedDictionaryIgnore.contains(propertyKey)
+      else { return nil };
+
+      return (propertyKey, value)
     };
     
-    return Dictionary(
+    var baseDict = Dictionary(
       uniqueKeysWithValues: propertyValueMap.compactMap { $0 }
     );
+    
+    Self.synthesizedDictionaryInlinedProperties.forEach {
+      guard let value = self[keyPath: $0] as? (any RNIDictionarySynthesizable)
+      else { return };
+      
+      baseDict =
+        baseDict.merging(value.synthesizedDictionary){ old, _ in old };
+    };
+    
+    return baseDict;
   };
 };
