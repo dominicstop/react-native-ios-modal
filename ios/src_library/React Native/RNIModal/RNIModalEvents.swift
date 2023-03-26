242d51409181707d1cb6155977fc7a193719de7c
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-03-27 06:28:36 +0800

    ⭐️ Impl: `RNIOnModalFocusEventData`
    Related:
    * TODO:2023-03-04-06-34-28 - Library Native Cleanup.

--- ios/src_library/React Native/RNIModal/RNIModalEvents.swift
+++ ios/src_library/React Native/RNIModal/RNIModalEvents.swift
@@ -24,3 +24,21 @@ public struct RNIModalBaseEventData: RNIDictionarySynthesizable {
   
   let modalData: RNIModalData;
 };
+
+public struct RNIOnModalFocusEventData: RNIDictionarySynthesizable {
+  
+  public static var synthesizedDictionaryIgnore: [String] {
+    ["modalData"]
+  };
+  
+  public static var synthesizedDictionaryInlinedProperties: [
+    PartialKeyPath<Self>
+  ] {
+    [\.modalData];
+  };
+  
+  let modalData: RNIModalBaseEventData;
+  let senderInfo: RNIModalData;
+  
+  let isInitial: Bool;
+};
