723d4ef588cc3308465420e92af770ace9d668f5
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-03-31 01:04:05 +0800

    ⭐️ Impl: `UIWindow+Helpers`
    Summary:
    * Rename `ios/src_library/Helpers+Utilities/UIWindow+WindowMetadata` to `UIWindow+Helpers`.
    * Update `UIWindow+Helpers` - Update `UIWindow` to conform to `RNIIdentifiable`, and remove old impl.

---
+++ ios/src_library/Helpers+Utilities/UIWindow+Helpers.swift
@@ -0,0 +1,12 @@
+//
+//  UIWindow+WindowMetadata.swift
+//  react-native-ios-modal
+//
+//  Created by Dominic Go on 3/30/23.
+//
+
+import Foundation
+
+extension UIWindow: RNIIdentifiable {
+  public static var synthesizedIdPrefix = "window-id-";
+};
