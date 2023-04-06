6307c00b0c5c52596dae0f9bf2f95aab7cbe15e5
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-04-07 05:36:09 +0800

    🛠 Refactor: Replace `RNIIdentifiable` `counterID`
    Summary: Replace `RNIIdentifiable`'s `counterID` with `Counter.typeToCounterMap`.

--- ios/src_library/Helpers+Utilities/RNIIdentifiable.swift
+++ ios/src_library/Helpers+Utilities/RNIIdentifiable.swift
@@ -7,15 +7,50 @@
 
 import Foundation
 
+
+fileprivate class Counter {
+  static var typeToCounterMap: Dictionary<String, Int> = [:];
+  
+  static func getTypeString(ofType _type: Any) -> String {
+    return String(describing: type(of: _type));
+  };
+  
+  static func set(forType type: Any, counter: Int) {
+    let typeString = Self.getTypeString(ofType: type);
+    Self.typeToCounterMap[typeString] = counter;
+  };
+  
+  static func set(forType typeString: String, counter: Int) {
+    Self.typeToCounterMap[typeString] = counter;
+  };
+  
+  static func get(forType type: Any) -> Int {
+    let typeString = Self.getTypeString(ofType: type);
+    
+    guard let counter = Self.typeToCounterMap[typeString] else {
+      Self.set(forType: typeString, counter: -1);
+      return -1;
+    };
+    
+    return counter;
+  };
+  
+  static func getAndIncrement(forType type: Any) -> Int {
+    let prevCount = Self.get(forType: type);
+    let nextCount = prevCount + 1;
+    
+    Self.set(forType: type, counter: nextCount);
+    return nextCount;
+  };
+};
+
 public final class RNIObjectIdentifier {
-  static var counterID = -1;
-  
-  let id: Int = {
-    RNIObjectIdentifier.counterID += 1;
-    return RNIObjectIdentifier.counterID;
-  }();
-  
+  let id: Int;
   let uuid = UUID();
+  
+  init(type: Any) {
+    self.id = Counter.getAndIncrement(forType: type);
+  };
 };
 
 public protocol RNIIdentifiable:
@@ -30,19 +65,20 @@ public protocol RNIIdentifiable:
 };
 
 extension RNIIdentifiable {
+  public static var synthesizedIdPrefix: String {
+    String(describing: Self.self) + "-";
+  };
    
   public var synthesizedIdentifier: RNIObjectIdentifier {
     if let identifier = self.metadata {
       return identifier;
     };
     
-    let identifier = RNIObjectIdentifier();
+    let identifier = RNIObjectIdentifier(type: Self.self);
     self.metadata = identifier;
     
     return identifier;
   };
-  
-  static var synthesizedIdPrefix: String { "" };
   
   public var synthesizedID: Int {
     self.synthesizedIdentifier.id;
