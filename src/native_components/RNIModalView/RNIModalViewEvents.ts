1db205f94de12bf599e5b75e4e26b56c961b06f0
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-04-15 18:49:13 +0800

    💫 Update: Types - `RNIModalData`
    Related:
    * `TODO:2023-03-04-13-15-11` - Refactor: Update Modal Events
    * `TODO:2023-03-30-15-52-42` - Update typescript types for modal event objects to match native event object.

--- src/native_components/RNIModalView/RNIModalViewEvents.ts
+++ src/native_components/RNIModalView/RNIModalViewEvents.ts
@@ -14,6 +14,7 @@ import type {
 export type RNIModalData = {
   modalNativeID: string;
   modalIndex: number;
+  modalIndexPrev: number;
   currentModalIndex: number;
   modalFocusState: ModalFocusState;
   modalFocusStatePref: ModalFocusState;
