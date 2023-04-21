dbf0e796d34de0b160553c9924600a66bdd7cef3
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-04-22 03:08:36 +0800

    💫 Update: Types - `UtilityTypes`
    Summary: Impl. `UnionWithAutoComplete`.

--- src/types/UtilityTypes.ts
+++ src/types/UtilityTypes.ts
@@ -1,3 +1,6 @@
 export type KeyMapType<T extends string, K extends { [k in `${T}`]: any }> = K;
 
 export type FunctionVoid = () => void;
+
+export interface Nothing {}
+export type UnionWithAutoComplete<T, U> = T | (U & Nothing);
