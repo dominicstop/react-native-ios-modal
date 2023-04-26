1332601c06005d465dba9620e91171f48f36e410
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-04-27 02:15:45 +0800

    🛠 Refactor: Split `RNIViewControllerLifeCycleNotifiable`
    Summary: Extract extension in `RNIViewControllerLifeCycleNotifiable` into its own file in `ios/src_library/React Native/RNIViewControllerLifeCycleNotifiable`.

--- ios/src_library/React Native/RNIViewControllerLifeCycleNotifiable/RNIViewControllerLifeCycleNotifiable.swift
+++ ios/src_library/React Native/RNIViewControllerLifeCycleNotifiable/RNIViewControllerLifeCycleNotifiable.swift
@@ -33,36 +33,3 @@ public protocol RNIViewControllerLifeCycleNotifiable: AnyObject {
   
 };
 
-extension RNIViewControllerLifeCycleNotifiable {
-  public func viewDidLoad(sender: UIViewController) {
-    // no-op
-  };
-  
-  public func viewDidLayoutSubviews(sender: UIViewController) {
-    // no-op
-  };
-  
-  public func viewWillAppear(sender: UIViewController, animated: Bool) {
-    // no-op
-  };
-  
-  public func viewDidAppear(sender: UIViewController, animated: Bool) {
-    // no-op
-  };
-  
-  public func viewWillDisappear(sender: UIViewController, animated: Bool) {
-    // no-op
-  };
-  
-  public func viewDidDisappear(sender: UIViewController, animated: Bool) {
-    // no-op
-  };
-  
-  public func willMove(sender: UIViewController, toParent parent: UIViewController?) {
-    // no-op
-  };
-  
-  public func didMove(sender: UIViewController, toParent parent: UIViewController?) {
-    // no-op
-  };
-};
