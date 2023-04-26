17c2ce10c4eb0976a3a933af89ae07c00742f15e
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-04-27 02:25:44 +0800

    🛠 Refactor: Split `RNIObjectMetadata`
    Summary: Extract extension in `RNIObjectMetadata` into its own file.

--- ios/src_library/React Native/RNIObjectMetadata/RNIObjectMetadata.swift
+++ ios/src_library/React Native/RNIObjectMetadata/RNIObjectMetadata.swift
@@ -7,29 +7,8 @@
 
 import Foundation
 
-fileprivate let RNIObjectMetadataMap = NSMapTable<AnyObject, AnyObject>(
-  keyOptions: .weakMemory,
-  valueOptions: .strongMemory
-);
-
 public protocol RNIObjectMetadata: AnyObject {
   associatedtype T: AnyObject;
   
   var metadata: T? { get set };
-};
-
-public extension RNIObjectMetadata {
-  var metadata: T? {
-    set {
-      if let newValue = newValue {
-        RNIObjectMetadataMap.setObject(newValue, forKey: self);
-        
-      } else {
-        RNIObjectMetadataMap.removeObject(forKey: self);
-      };
-    }
-    get {
-      RNIObjectMetadataMap.object(forKey: self) as? T
-    }
-  };
 };
