9c2771d50acb48981b30b7951d676cdb8ac125a5
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-05-01 16:20:27 +0800

    ⭐️ Impl: Add `CAAnimation+Helpers`

---
+++ ios/src_library/Extensions/CAAnimation+Helpers.swift
@@ -0,0 +1,17 @@
+//
+//  CAAnimation+Helpers.swift
+//  react-native-ios-modal
+//
+//  Created by Dominic Go on 5/1/23.
+//
+
+import Foundation
+
+public extension CAAnimation {
+  func waitUntiEnd(_ block: @escaping () -> Void){
+    DispatchQueue.main.asyncAfter(
+      deadline: .now() + self.duration,
+      execute: block
+    );
+  };
+};
