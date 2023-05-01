43c299a31b3a3c2e9d3ada0a9b54439fd255b368
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2023-05-02 06:56:48 +0800

    ⭐️ Impl: Add `Encodable+Helpers`

---
+++ ios/src_library/Extensions/Encodable+Helpers.swift
@@ -0,0 +1,31 @@
+//
+//  Encodable+Helpers.swift
+//  react-native-ios-modal
+//
+//  Created by Dominic Go on 5/2/23.
+//
+
+import Foundation
+
+
+public extension Encodable {
+
+  var asJsonData: Data? {
+    let encoder = JSONEncoder();
+    encoder.outputFormatting = .prettyPrinted;
+    encoder.dateEncodingStrategy = .iso8601;
+    
+    return try? encoder.encode(self);
+  };
+  
+  var asDictionary : [String: Any]? {
+    guard let jsonData = self.asJsonData,
+          let json = try? JSONSerialization.jsonObject(
+            with: jsonData,
+            options: []
+          )
+    else { return nil };
+    
+    return json as? [String: Any];
+  };
+};
