6fffcf9ce025b064ce7c16f83c94607a2a791630
Dominic Go <dominic@dominicgo.dev>
2024-09-27 15:33:28 +0800

    ⭐️ Impl: `DictionaryRepresentable`

---
+++ ios/Temp/DictionaryRepresentable.swift
@@ -0,0 +1,12 @@
+//
+//  DictionaryRepresentable.swift
+//  react-native-ios-modal
+//
+//  Created by Dominic Go on 9/27/24.
+//
+
+import Foundation
+
+public protocol DictionaryRepresentable {
+  var asDictionary: [String: Any] { get };
+};
