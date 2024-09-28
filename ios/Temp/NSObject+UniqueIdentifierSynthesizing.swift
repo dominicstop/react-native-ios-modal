8a01e80e38e65a25d8e889d1e0862049782734cc
Dominic Go <dominic@dominicgo.dev>
2024-09-29 06:04:33 +0800

    🛠 Refactor: Replace w/ `NSObject+UniqueIdentifierSynthesizing`

---
+++ ios/Temp/NSObject+UniqueIdentifierSynthesizing.swift
@@ -0,0 +1,15 @@
+//
+//  NSObject+UniqueIdentifierSynthesizing.swift
+//  react-native-ios-modal
+//
+//  Created by Dominic Go on 9/29/24.
+//
+
+import Foundation
+import DGSwiftUtilities
+
+
+extension NSObject: UniqueIdentifierSynthesizing {
+  // empty requirements
+};
+
